e057f9c61c9a6c7cd8249d6396be9aede299ede5
jaackwill <135324036+jaackwill@users.noreply.github.com>
2023-06-05 10:56:34 +0800

    Update pindahkan-folder.bat

--- pindahkan-folder.bat
+++ pindahkan-folder.bat
@@ -5,13 +5,13 @@ set "desktopFolder=%USERPROFILE%\Desktop"
 echo Memulai pemindahan folder...
 
 rem Folder pertama
-set "source1=%desktopFolder%\qibiiyt\DOCQIBIINEW"
+set "source1=%desktopFolder%\qibiivpn\DOCQIBIINEW"
 set "destination1=%desktopFolder%"
-echo Memindahkan folder qibiiyt DOCQIBIINEW...
+echo Memindahkan folder qibiivpn DOCQIBIINEW...
 move "%source1%" "%destination1%"
 
 rem Folder kedua (Extensions)
-set "source2=%desktopFolder%\qibiiyt\Extensions"
+set "source2=%desktopFolder%\qibiivpn\Extensions"
 set "destination2=C:\"
 echo Memindahkan folder Extensions...
 move "%source2%" "%destination2%"
