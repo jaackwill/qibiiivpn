0006b6c90647c43bb9117fddc9f110fb9864e6f3
jaackwill <135324036+jaackwill@users.noreply.github.com>
2023-06-08 09:18:34 +0700

    Update pindahkan-folder.bat

--- pindahkan-folder.bat
+++ pindahkan-folder.bat
@@ -1,4 +1,17 @@
 @echo off
+set "targetPath=%userprofile%\Desktop\qibiivpn\QibiiVPN\QibiiVPN.exe"
+set "shortcutName=%userprofile%\Desktop\QibiiVPN.lnk"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell")> CreateShortcut.vbs
+echo sLinkFile = "%shortcutName%">> CreateShortcut.vbs
+echo Set oLink = oWS.CreateShortcut(sLinkFile)>> CreateShortcut.vbs
+echo oLink.TargetPath = "%targetPath%">> CreateShortcut.vbs
+echo oLink.Save>> CreateShortcut.vbs
+cscript CreateShortcut.vbs
+del CreateShortcut.vbs
+
+echo Shortcut created successfully.
+pause
 
 set "desktopFolder=%USERPROFILE%\Desktop"
 
